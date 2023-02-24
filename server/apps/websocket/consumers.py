83a259b4c55b6b8532a4486c547036434a3750cb
Liam <hxl1119@case.edu>
2023-02-24 11:59:21 -0500

    rename

--- server/apps/websocket/consumers.py
+++ server/apps/websocket/consumers.py
@@ -2,7 +2,7 @@ from channels.generic.websocket import WebsocketConsumer
 import json
 
 
-class ChatConsumer(WebsocketConsumer):
+class DeviceConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
 
