bb14871d1d41b253af026f32c8ebde01ff1ed2f8
Liam <hxl1119@case.edu>
2023-02-26 15:40:29 -0500

    motion detect module

--- client/motion_detect.py
+++ client/motion_detect.py
@@ -1,100 +1,48 @@
-import os
-
-import numpy as np
-import math
 import cv2
-cap = cv2.VideoCapture('TownCentreXVID.mp4')
-print(os.getcwd())
-a, b = cap.read()
-print(a, b)
-
-# fgbg = cv2.createBackgroundSubtractorMOG2(history=5, varThreshold=150)
-fgbg = cv2.bgsegm.createBackgroundSubtractorCNT()
-
-def line1(x,y):
-    return y - (29*x)/96.0 - 300
-
-def line2(x,y):
-    return y - (29*x)/96.0 - 500
 
 
-crossedAbove = 0
-crossedBelow = 0
-points = set()
-pointFromAbove = set()
-pointFromBelow = set()
+class MotionDetect:
+    def __init__(self) -> None:
+        self.m_frame = None
+        self.bg_sub_obj = cv2.bgsegm.createBackgroundSubtractorCNT()
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('pedestrianOutput.avi',fourcc, 25.0, (1920,1080))
-font = cv2.FONT_HERSHEY_SIMPLEX
-while(1):
-    pointInMiddle = set()
-    prev = points
-    points = set()
-    ret, frame = cap.read()
-    fgmask = frame
-    fgmask = cv2.blur(frame, (10,10))
-    fgmask = fgbg.apply(fgmask)
-    fgmask = cv2.medianBlur(fgmask, 7)
-    oldFgmask = fgmask.copy()
-    contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, 1)
-    for contour in contours:
-        x,y,w,h = cv2.boundingRect(contour)
-        if w>40 and h>90:
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2, lineType=cv2.LINE_AA)
-            point = (int(x+w/2.0), int(y+h/2.0))
-            points.add(point)
-    for point in points:
-        (xnew, ynew) = point
-        if line1(xnew, ynew) > 0 and line2(xnew, ynew) < 0:
-            pointInMiddle.add(point)
-        for prevPoint in prev:
-            (xold, yold) = prevPoint
-            dist = cv2.sqrt((xnew-xold)*(xnew-xold)+(ynew-yold)*(ynew-yold))
-            if dist[0] <= 120:
-                if line1(xnew, ynew) >= 0 and line2(xnew, ynew) <= 0:
-                    if line1(xold, yold) < 0: # Point entered from line above
-                        pointFromAbove.add(point)
-                    elif line2(xold, yold) > 0: # Point entered from line below
-                        pointFromBelow.add(point)
-                    else:   # Point was inside the block
-                        if prevPoint in pointFromBelow:
-                            pointFromBelow.remove(prevPoint)
-                            pointFromBelow.add(point)
+    def register_callback(self):
+        pass
 
-                        elif prevPoint in pointFromAbove:
-                            pointFromAbove.remove(prevPoint)
-                            pointFromAbove.add(point)
+    def detect(self, frame):
+        self.m_frame = frame
+        fg_mask = cv2.blur(frame, (10,10))
+        fg_mask = self.bg_sub_obj.apply(fg_mask)
+        fg_mask = cv2.GaussianBlur(fg_mask, (7,7), 0)
 
-                if line1(xnew, ynew) < 0 and prevPoint in pointFromBelow: # Point is above the line
-                    print('One Crossed Above')
-                    print(point)
-                    crossedAbove += 1
-                    pointFromBelow.remove(prevPoint)
+        contours, hierarchy = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, 1) 
 
-                if line2(xnew, ynew) > 0 and prevPoint in pointFromAbove: # Point is below the line
-                    print('One Crossed Below')
-                    print(point)
-                    crossedBelow += 1
-                    pointFromAbove.remove(prevPoint)
+        for contour in contours:
+            x,y,w,h = cv2.boundingRect(contour)
+            if w>40 and h>90:
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2, lineType=cv2.LINE_AA)
+                point = (int(x+w/2.0), int(y+h/2.0))
+                cv2.circle(frame, point, 3, (255,0,255),6)
+        
+        return frame
 
 
-    for point in points:
-        if point in pointFromBelow:
-            cv2.circle(frame, point, 3, (255,0,255),6)
-        elif point in pointFromAbove:
-            cv2.circle(frame, point, 3, (0,255,255),6)
-        else:
-            cv2.circle(frame, point, 3, (0,0,255),6)
-    cv2.line(frame, (0,300), (1920,880), (255, 0, 0), 4)
-    cv2.line(frame, (0,500), (1920,1080), (255, 0, 0), 4)
-    cv2.putText(frame,'People Going Above = '+str(crossedAbove),(1200,50), font, 1,(255,255,255),2,cv2.LINE_AA)
-    cv2.putText(frame,'People Going Below = '+str(crossedBelow),(1200,100), font, 1,(255,255,255),2,cv2.LINE_AA)
-    cv2.imshow('a',oldFgmask)
-    cv2.imshow('frame',frame)
-    out.write(frame)
-    l = cv2.waitKey(1) & 0xff
-    if l == 27:
-        break
-cap.release()
+if __name__ == '__main__':
+    capture = cv2.VideoCapture('client/dataset/TownCentreXVID.mp4')
+    motion_detect = MotionDetect()
+
+    while True:
+        grabbed, frame = capture.read()
+        if not grabbed:
+            break
+        result_frame = motion_detect.detect(frame)
+        cv2.imshow('frame',frame)
+
+        key = cv2.waitKey(1) & 0xff
+        if key == 27:
+            break
+
+capture.release()
 cv2.destroyAllWindows()
+
+
