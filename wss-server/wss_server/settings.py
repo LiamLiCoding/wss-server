fd2f70bdacd7f50926399bcd93f604229babef61
Liam <hxl1119@case.edu>
2023-03-04 15:52:50 -0500

    fix bugs

--- wss-server/wss_server/settings.py
+++ wss-server/wss_server/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.email_control',
+    'apps.email',
     'apps.api_control',
     'apps.api_websocket',
     'apps.record',
