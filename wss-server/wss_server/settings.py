d1f672261e0ec8793d484462a11af95e8e688dd1
Liam <hxl1119@case.edu>
2023-03-30 16:51:51 -0400

    debug = False

--- wss-server/wss_server/settings.py
+++ wss-server/wss_server/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = key_define.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
